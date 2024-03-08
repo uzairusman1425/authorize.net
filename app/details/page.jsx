dbe05d790c2ab435c4b7cb0c21fcf8c8cb307b81
uzairusman1425 <uzairusman1425@gmail.com>
2024-03-09 01:38:45 +0500

    adsas

--- app/details/page.jsx
+++ app/details/page.jsx
@@ -86,7 +86,7 @@ function DetailsForm() {
     } else {
       try {
         let res = await fetch(
-          "https://apitest.authorize.net/xml/v1/request.api",
+          "https://api.authorize.net/xml/v1/request.api",
           {
             method: "POST",
             body: data,
