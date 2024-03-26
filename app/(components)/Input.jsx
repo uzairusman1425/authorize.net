fbee4a442db18afddca9450f55276d9f2189f97f
SHERRY699 <shehryarnasir98@gmail.com>
2024-03-26 22:20:08 +0500

    ui changes

--- app/(components)/Input.jsx
+++ app/(components)/Input.jsx
@@ -2,14 +2,14 @@
 function Input(props) {
   return (
     <>
-      <div className="input1 ml-4">
+      <div className="input1 ml-4 ">
         <p className="text-xl font-semibold  mb-2">{props.detail}</p>
         <input
           type={props.type}
           name={props.name}
           value={props.state}
           id={props.id}
-          className="border-2 px-2  border-black bg-transparent rounded-md w-[400px] h-[40px]"
+          className="border-2 px-2  border-black bg-transparent rounded-md w-[300px] h-[40px]"
           placeholder={props.placeholder}
           onChange={(e) => props.setstate(e.target.value)}
         />
--- app/details/page.jsx
+++ app/details/page.jsx
@@ -87,6 +87,7 @@ function DetailsForm() {
   return (
     <>
       <Nnavbar />
+
       <section
         className="w-full h-full flex justify-center items-center  mb-2  "
         style={{ backgroundImage: "url('/background.jpg')" }}
@@ -96,141 +97,232 @@ function DetailsForm() {
           className="w-[600px] h-full mt-20  bg-transparent shadow-xl bg-white border-2  justify-center items-center gap-10 flex flex-col pb-4 "
         >
           <h1 className="text-center text-3xl font-bold">DETAILS</h1>
-          <Input
-            type="text"
-            name="first name"
-            id="fname"
-            placeholder="Enter First Name"
-            detail="First Name"
-            state={firstname}
-            setstate={setFirstname}
-          />
-          <Input
-            type="text"
-            name="last name"
-            id="lname"
-            placeholder="Enter Last Name"
-            detail="Last Name"
-            state={lastname}
-            setstate={setLastname}
-          />
+          <div className="payment-info  w-full h-full text-center">
+            <h1 className="font-bold text-2xl border-2 bg-gray-400 border-gray-400">
+              Payment/Authorization Information
+            </h1>
+            <p className="text-lg mt-2">
+              Accepted Payment Method Visa, MasterCard, American Express,
+              Discover, JCB, eCheck
+            </p>
+            <Input
+              type="number"
+              name="cardnumber"
+              id="cardnumber"
+              placeholder="Enter Your Card Number"
+              detail="Card  Number(enter without spaces) * "
+              state={cardnumber}
+              setstate={setCardnumber}
+            />
 
-          <Input
-            type="number"
-            name="cardnumber"
-            id="cardnumber"
-            placeholder="Enter Your Card Number"
-            detail="Card  Number"
-            state={cardnumber}
-            setstate={setCardnumber}
-          />
-          <Input
-            type="number"
-            name="cardcode"
-            id="cardcode"
-            placeholder="Enter Your Card Code"
-            detail="Card Code"
-            state={cardcode}
-            setstate={setCardcode}
-          />
-          <Input
-            type="text"
-            name="expiry"
-            id="expiry"
-            detail="Expiry Date"
-            placeholder="Enter Your Expiry Date"
-            state={expiry}
-            setstate={setExpiry}
-          />
+            <Input
+              type="text"
+              name="expiry"
+              id="expiry"
+              detail="Expiry Date 
+            (mmyy) * "
+              placeholder="Enter Your Expiry Date"
+              state={expiry}
+              setstate={setExpiry}
+            />
+            <Input
+              type="number"
+              name="Amount"
+              id="number"
+              placeholder="Enter Your Amount"
+              detail="Amount  (i.e.,10.00) * "
+              state={amount}
+              setstate={setAmount}
+            />
 
-          <Input
-            type="text"
-            name="invoice"
-            id="invoice"
-            placeholder="Enter Your Invoice Number"
-            detail="Invoice Number"
-            state={invoiceno}
-            setstate={setInvoiceno}
-          />
+            <Input
+              type="number"
+              name="cardcode"
+              id="cardcode"
+              placeholder="Enter Your Card Code"
+              detail="Card Code"
+              state={cardcode}
+              setstate={setCardcode}
+            />
+          </div>
+          <div className="order-info  w-full h-full text-center">
+            <h1 className="font-bold text-2xl border-2 bg-gray-400 border-gray-400">
+              Order Information
+            </h1>
 
-          <Input
-            type="number"
-            name="Amount"
-            id="number"
-            placeholder="Enter Your Amount"
-            detail="Amount"
-            state={amount}
-            setstate={setAmount}
-          />
+            <Input
+              type="text"
+              name="invoice"
+              id="invoice"
+              placeholder="Enter Your Invoice Number"
+              detail="Invoice Number"
+              state={invoiceno}
+              setstate={setInvoiceno}
+            />
+            <Input
+              type="text"
+              name="desc"
+              id="desc"
+              placeholder="Enter  Description"
+              detail="Description"
+              state={description}
+              setstate={setDescription}
+            />
+          </div>
 
-          <Input
-            type="text"
-            name="address"
-            id="address"
-            placeholder="Enter Your Address"
-            detail="Address"
-            state={address}
-            setstate={setAddress}
-          />
+          <div className="Customer-info  w-full h-full text-center">
+            <h1 className="font-bold text-2xl border-2 bg-gray-400 border-gray-400">
+              Customer Billing Information
+            </h1>
+            <Input
+              type="text"
+              name="first name"
+              id="fname"
+              placeholder="Enter First Name"
+              detail="First Name"
+              state={firstname}
+              setstate={setFirstname}
+            />
+            <Input
+              type="text"
+              name="last name"
+              id="lname"
+              placeholder="Enter Last Name"
+              detail="Last Name"
+              state={lastname}
+              setstate={setLastname}
+            />
+            <Input
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Enter Your Address"
+              detail="Address"
+              state={address}
+              setstate={setAddress}
+            />
 
-          <Input
-            type="text"
-            name="desc"
-            id="desc"
-            placeholder="Enter  Description"
-            detail="Description"
-            state={description}
-            setstate={setDescription}
-          />
+            <Input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Enter Your City"
+              detail="City"
+              state={city}
+              setstate={setCity}
+            />
 
-          <Input
-            type="text"
-            name="city"
-            id="city"
-            placeholder="Enter Your City"
-            detail="City"
-            state={city}
-            setstate={setCity}
-          />
+            <Input
+              type="text"
+              name="state"
+              id="state"
+              placeholder="Enter Your State"
+              detail="State"
+              state={state}
+              setstate={setState}
+            />
+            <Input
+              type="number"
+              name="zipcode"
+              id="zipcode"
+              placeholder="Enter Your Zip Code"
+              detail="Zip Code"
+              state={zipcode}
+              setstate={setZipcode}
+            />
 
-          <Input
-            type="text"
-            name="state"
-            id="state"
-            placeholder="Enter Your State"
-            detail="State"
-            state={state}
-            setstate={setState}
-          />
-          <Input
-            type="number"
-            name="zipcode"
-            id="zipcode"
-            placeholder="Enter Your Zip Code"
-            detail="Zip Code"
-            state={zipcode}
-            setstate={setZipcode}
-          />
+            <Input
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="Enter Your Phone No"
+              detail="Phone Number"
+              state={phoneno}
+              setstate={setPhoneno}
+            />
 
-          <Input
-            type="number"
-            name="phone"
-            id="phone"
-            placeholder="Enter Your Phone No"
-            detail="Phone Number"
-            state={phoneno}
-            setstate={setPhoneno}
-          />
+            <Input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Enter Your Country"
+              detail="Country"
+              state={country}
+              setstate={setCountry}
+            />
+          </div>
 
-          <Input
-            type="text"
-            name="country"
-            id="country"
-            placeholder="Enter Your Country"
-            detail="Country"
-            state={country}
-            setstate={setCountry}
-          />
+          <div className="Shipping-info  w-full h-full text-center">
+            <h1 className="font-bold text-2xl border-2 bg-gray-400 border-gray-400">
+              Shipping Information
+            </h1>
+            <Input
+              type="text"
+              name="first name"
+              id="fname"
+              placeholder="Enter First Name"
+              detail="First Name"
+              state={firstname}
+              setstate={setFirstname}
+            />
+            <Input
+              type="text"
+              name="last name"
+              id="lname"
+              placeholder="Enter Last Name"
+              detail="Last Name"
+              state={lastname}
+              setstate={setLastname}
+            />
+            <Input
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Enter Your Address"
+              detail="Address"
+              state={address}
+              setstate={setAddress}
+            />
+
+            <Input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Enter Your City"
+              detail="City"
+              state={city}
+              setstate={setCity}
+            />
+
+            <Input
+              type="text"
+              name="state"
+              id="state"
+              placeholder="Enter Your State"
+              detail="State"
+              state={state}
+              setstate={setState}
+            />
+            <Input
+              type="number"
+              name="zipcode"
+              id="zipcode"
+              placeholder="Enter Your Zip Code"
+              detail="Zip Code"
+              state={zipcode}
+              setstate={setZipcode}
+            />
+
+            <Input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Enter Your Country"
+              detail="Country"
+              state={country}
+              setstate={setCountry}
+            />
+          </div>
 
           <button className="w-[200px] py-2 border-2 font-bold bg-green-400 text-white rounded-lg">
             Submit
