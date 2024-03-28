80a302da08f12bd39241e25131321c6658475cd0
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-03-28 22:01:55 +0500

    test

--- app/api/authorize/route.js
+++ app/api/authorize/route.js
@@ -1,186 +1,197 @@
-import { NextResponse } from "next/server";
-const ApiContracts = require("authorizenet").APIContracts;
-const ApiControllers = require("authorizenet").APIControllers;
+import { NextResponse } from "next/server"
+const ApiContracts = require("authorizenet").APIContracts
+const ApiControllers = require("authorizenet").APIControllers
 
 export async function POST(req) {
-  let error = "";
-  let errorText = "";
-  try {
-    // Extract data from the request body
-    const {
-      firstname,
-      lastname,
-      cardnumber,
-      expiry,
-      invoiceno,
-      amount,
-      address,
-      description,
-      city,
-      state,
-      zipcode,
-      country,
-      cardcode,
-    } = await req.json();
+	let error = ""
+	let errorText = ""
+	try {
+		// Extract data from the request body
+		const {
+			firstname,
+			lastname,
+			cardnumber,
+			expiry,
+			invoiceno,
+			amount,
+			address,
+			description,
+			city,
+			state,
+			zipcode,
+			country,
+			cardcode
+		} = await req.json()
 
-    // Construct transaction request
-    const merchantAuthenticationType =
-      new ApiContracts.MerchantAuthenticationType();
-    merchantAuthenticationType.setName(process.env.NEXT_PUBLIC_API_LOGIN_ID);
-    merchantAuthenticationType.setTransactionKey(
-      process.env.NEXT_PUBLIC_TRANSACTION_KEY
-    );
+		console.log(
+			process.env.NEXT_PUBLIC_API_LOGIN_ID,
+			process.env.NEXT_PUBLIC_TRANSACTION_KEY
+		)
 
-    const creditCard = new ApiContracts.CreditCardType();
-    creditCard.setCardNumber(cardnumber);
-    creditCard.setExpirationDate(expiry);
-    creditCard.setCardCode(cardcode);
+		// Construct transaction request
+		const merchantAuthenticationType =
+			new ApiContracts.MerchantAuthenticationType()
+		merchantAuthenticationType.setName(process.env.NEXT_PUBLIC_API_LOGIN_ID)
+		merchantAuthenticationType.setTransactionKey(
+			process.env.NEXT_PUBLIC_TRANSACTION_KEY
+		)
 
-    const paymentType = new ApiContracts.PaymentType();
-    paymentType.setCreditCard(creditCard);
+		const creditCard = new ApiContracts.CreditCardType()
+		creditCard.setCardNumber(cardnumber)
+		creditCard.setExpirationDate(expiry)
+		creditCard.setCardCode(cardcode)
 
-    const orderDetails = new ApiContracts.OrderType();
-    orderDetails.setInvoiceNumber(invoiceno);
-    orderDetails.setDescription(description);
+		const paymentType = new ApiContracts.PaymentType()
+		paymentType.setCreditCard(creditCard)
 
-    const tax = new ApiContracts.ExtendedAmountType();
-    tax.setAmount("0");
-    tax.setName("NA");
-    tax.setDescription(description);
+		const orderDetails = new ApiContracts.OrderType()
+		orderDetails.setInvoiceNumber(invoiceno)
+		orderDetails.setDescription(description)
 
-    const duty = new ApiContracts.ExtendedAmountType();
-    duty.setAmount("0");
-    duty.setName("NA");
-    duty.setDescription(description);
+		const tax = new ApiContracts.ExtendedAmountType()
+		tax.setAmount("0")
+		tax.setName("NA")
+		tax.setDescription(description)
 
-    const shipping = new ApiContracts.ExtendedAmountType();
-    shipping.setAmount("0");
-    shipping.setName("NA");
-    shipping.setDescription(description);
+		const duty = new ApiContracts.ExtendedAmountType()
+		duty.setAmount("0")
+		duty.setName("NA")
+		duty.setDescription(description)
 
-    const billTo = new ApiContracts.CustomerAddressType();
-    billTo.setFirstName(firstname);
-    billTo.setLastName(lastname);
-    billTo.setCompany("Brainiacs Lab");
-    billTo.setAddress(address);
-    billTo.setCity(city);
-    billTo.setState(state);
-    billTo.setZip(zipcode);
-    billTo.setCountry(country);
+		const shipping = new ApiContracts.ExtendedAmountType()
+		shipping.setAmount("0")
+		shipping.setName("NA")
+		shipping.setDescription(description)
 
-    const shipTo = new ApiContracts.CustomerAddressType();
-    shipTo.setFirstName(firstname);
-    shipTo.setLastName(lastname);
-    shipTo.setCompany("Brainiacs Lab");
-    shipTo.setAddress(address);
-    shipTo.setCity(city);
-    shipTo.setState(state);
-    shipTo.setZip(zipcode);
-    shipTo.setCountry(country);
+		const billTo = new ApiContracts.CustomerAddressType()
+		billTo.setFirstName(firstname)
+		billTo.setLastName(lastname)
+		billTo.setCompany("Brainiacs Lab")
+		billTo.setAddress(address)
+		billTo.setCity(city)
+		billTo.setState(state)
+		billTo.setZip(zipcode)
+		billTo.setCountry(country)
 
-    // Construct the transaction request
-    const transactionRequestType = new ApiContracts.TransactionRequestType();
-    transactionRequestType.setTransactionType(
-      ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION
-    );
-    transactionRequestType.setPayment(paymentType);
-    transactionRequestType.setAmount(amount);
-    transactionRequestType.setOrder(orderDetails);
-    transactionRequestType.setTax(tax);
-    transactionRequestType.setDuty(duty);
-    transactionRequestType.setShipping(shipping);
-    transactionRequestType.setBillTo(billTo);
-    transactionRequestType.setShipTo(shipTo);
+		const shipTo = new ApiContracts.CustomerAddressType()
+		shipTo.setFirstName(firstname)
+		shipTo.setLastName(lastname)
+		shipTo.setCompany("Brainiacs Lab")
+		shipTo.setAddress(address)
+		shipTo.setCity(city)
+		shipTo.setState(state)
+		shipTo.setZip(zipcode)
+		shipTo.setCountry(country)
 
-    const createRequest = new ApiContracts.CreateTransactionRequest();
-    createRequest.setMerchantAuthentication(merchantAuthenticationType);
-    createRequest.setTransactionRequest(transactionRequestType);
+		// Construct the transaction request
+		const transactionRequestType = new ApiContracts.TransactionRequestType()
+		transactionRequestType.setTransactionType(
+			ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION
+		)
+		transactionRequestType.setPayment(paymentType)
+		transactionRequestType.setAmount(amount)
+		transactionRequestType.setOrder(orderDetails)
+		transactionRequestType.setTax(tax)
+		transactionRequestType.setDuty(duty)
+		transactionRequestType.setShipping(shipping)
+		transactionRequestType.setBillTo(billTo)
+		transactionRequestType.setShipTo(shipTo)
 
-    // Create a promise to handle the Authorize.Net request
-    const authorizeNetPromise = new Promise((resolve, reject) => {
-      const ctrl = new ApiControllers.CreateTransactionController(
-        createRequest.getJSON()
-      );
-      ctrl.execute(() => {
-        const apiResponse = ctrl.getResponse();
-        const response = new ApiContracts.CreateTransactionResponse(
-          apiResponse
-        );
-        const message = response?.getTransactionResponse()?.getMessages()
-          ?.message[0];
-        const messageText = message?.description;
-        console.log(response.messages.message[0]);
-        
+		const createRequest = new ApiContracts.CreateTransactionRequest()
+		createRequest.setMerchantAuthentication(merchantAuthenticationType)
+		createRequest.setTransactionRequest(transactionRequestType)
 
-        error = response?.getTransactionResponse()?.getErrors()?.error[0];
-        errorText = error?.errorText;
-        console.log(error);
+		// Create a promise to handle the Authorize.Net request
+		const authorizeNetPromise = new Promise((resolve, reject) => {
+			const ctrl = new ApiControllers.CreateTransactionController(
+				createRequest.getJSON()
+			)
+			ctrl.execute(() => {
+				const apiResponse = ctrl.getResponse()
+				const response = new ApiContracts.CreateTransactionResponse(
+					apiResponse
+				)
+				const message = response
+					?.getTransactionResponse()
+					?.getMessages()?.message[0]
+				const messageText = message?.description
+				console.log(response.messages.message[0])
 
-        // Handle the response from Authorize.Net
-        if (
-          response != null &&
-          response.getMessages().getResultCode() ==
-            ApiContracts.MessageTypeEnum.OK
-        ) {
-          if (response.getTransactionResponse().getMessages() != null) {
-            // Successful transaction
-            resolve(
-              NextResponse.json({
-                message: messageText,
-                status: 200,
-              })
-            );
-          } else {
-            // Failed transaction
-            if (response.getTransactionResponse().getErrors() != null) {
-              reject(
-                NextResponse.json({
-                  message: errorText,
-                  status: 504,
-                })
-              );
-            } else {
-              reject(
-                NextResponse.json({
-                  message: errorText,
-                  status: 508,
-                })
-              );
-            }
-          }
-        } else {
-          // Failed transaction
-          if (
-            response != null &&
-            response.getTransactionResponse() != null &&
-            response.getTransactionResponse().getErrors() != null
-          ) {
-            reject(
-              NextResponse.json({
-                message: errorText,
-                status: 505,
-              })
-            );
-          } else {
-            reject(
-              NextResponse.json({
-                message: errorText,
-                status: 506,
-              })
-            );
-          }
-        }
-      });
-    });
+				error = response?.getTransactionResponse()?.getErrors()
+					?.error[0]
+				errorText = error?.errorText
+				console.log(error)
 
-    // Wait for the promise to resolve or reject
-    const result = await authorizeNetPromise;
-    return result;
-  } catch (error) {
-    // Handle errors
-    return NextResponse.json({
-      message: errorText,
-      status: 504,
-    });
-  }
+				// Handle the response from Authorize.Net
+				if (
+					response != null &&
+					response.getMessages().getResultCode() ==
+						ApiContracts.MessageTypeEnum.OK
+				) {
+					if (
+						response.getTransactionResponse().getMessages() != null
+					) {
+						// Successful transaction
+						resolve(
+							NextResponse.json({
+								message: messageText,
+								status: 200
+							})
+						)
+					} else {
+						// Failed transaction
+						if (
+							response.getTransactionResponse().getErrors() !=
+							null
+						) {
+							reject(
+								NextResponse.json({
+									message: errorText,
+									status: 504
+								})
+							)
+						} else {
+							reject(
+								NextResponse.json({
+									message: errorText,
+									status: 508
+								})
+							)
+						}
+					}
+				} else {
+					// Failed transaction
+					if (
+						response != null &&
+						response.getTransactionResponse() != null &&
+						response.getTransactionResponse().getErrors() != null
+					) {
+						reject(
+							NextResponse.json({
+								message: errorText,
+								status: 505
+							})
+						)
+					} else {
+						reject(
+							NextResponse.json({
+								message: errorText,
+								status: 506
+							})
+						)
+					}
+				}
+			})
+		})
+
+		// Wait for the promise to resolve or reject
+		const result = await authorizeNetPromise
+		return result
+	} catch (error) {
+		// Handle errors
+		return NextResponse.json({
+			message: errorText,
+			status: 504
+		})
+	}
 }
